22b6c62fc1a6a4243932e9b1589dfb15a70b030a
GreenAppleDan <denibogda@gmail.com>
2022-03-12 15:53:27 +0400

    Initial setup of the project

---
+++ Shortly/Shortly/Presentation/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Shortly
+//
+//  Created by Denis on 12.03.2022.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
